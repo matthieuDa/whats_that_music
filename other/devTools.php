34ba59ffb56e6166a9180b31e6889d975be69fdb
Matthieu DALLE <dalle@intechinfo.fr>
2019-12-02 13:13:44 +0100

    For Eyal

---
+++ other/devTools.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Developement tools</title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<!-- FontAwesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
+
+	<style type="text/css">
+		.padding 	 				{ padding 		: 2em			; 	}
+		.center 	 				{ text-align 	: center		; 	}
+		.smallerText 				{ font-size 	: small			; 	}
+		.row	 	 				{ margin-top 	: 4em		 	;	}
+		.center 	 				{ text-align 	: center		;
+					   				  display       : block			;
+					   				  margin        : auto			;	}
+		.black		 				{ color 		: black			; 	}
+		.fab.fa-github 				{ color 		: #24292E		; 	}
+		.fab.fa-font-awesome-flag 	{ color 		: #329AF0		; 	}
+		.fab.fa-bootstrap 			{ color 		: #563C7C		; 	}
+		.fab.fa-php		 			{ color 		: #777CB5		; 	}
+		.fab.fa-html5 	 			{ color 		: #E34E26		; 	}
+		.fab.fa-css3 	 			{ color 		: #1E4494		; 	}
+		.fab.fa-js		 			{ color 		: #F7E018		; 	}
+		.fab.fa-angular		 		{ color 		: #C3012F		; 	}
+		label 						{ font-size 	: x-large		;
+									  margin-top 	: 1em			;	}
+		.return					 	{ position		: absolute		;
+									  top 			: 3%			;
+									  left 			: 2%			; 	}
+	</style>
+</head>
+<body>
+	<div class="padding">
+		<h1 class="center">Here are some web development tools for developing your projects.</h1>
+		<a href="./index.php" class="return"><i class="fas fa-arrow-alt-circle-left fa-4x"></i></a>
+		<div class="container">
+			<div class="row">
+			    <div class="col-sm center"><a href="https://github.com/">
+			    	<i class="fab fa-github fa-10x"></i></br>
+			    	<label class="black">Github</label>
+			    </a></div>
+			    <div class="col-sm center"><a href="https://fontawesome.com/">
+			    	<i class="fab fa-font-awesome-flag fa-10x"></i></br>
+			    	<label class="black">Font Awesome</label>
+			    </a></div>
+			    <div class="col-sm center"><a href="https://getbootstrap.com/docs/4.4/getting-started/introduction/">
+			    	<i class="fab fa-bootstrap fa-10x"></i></br>
+			    	<label class="black">Bootstrap</label>
+			    </a></div>
+			</div>
+			<div class="row">
+			    <div class="col-sm center"><a href="https://www.php.net/docs.php">
+			    	<i class="fab fa-php fa-10x"></i></br>
+			    	<label class="black">PHP Documentation</label>
+			    </a></div>
+			    <div class="col-sm center"><a href="https://developer.mozilla.org/fr/docs/Web/HTML">
+			    	<i class="fab fa-html5 fa-10x"></i></br>
+			    	<label class="black">HTML Documentation</label>
+			    </a></div>
+			    <div class="col-sm center"><a href="https://developer.mozilla.org/fr/docs/Web/CSS/Reference">
+			    	<i class="fab fa-css3 fa-10x"></i></br>
+			    	<label class="black">CSS Documentation</label>
+			    </a></div>
+			</div>
+			<div class="row">
+			    <div class="col-sm center"><a href="https://developer.mozilla.org/fr/docs/Web/JavaScript">
+			    	<i class="fab fa-js fa-10x"></i></br>
+			    	<label class="black">JavaScript Documentation</label>
+			    </a></div>
+			    <div class="col-sm center"><a href="https://docs.angularjs.org/api">
+			    	<i class="fab fa-angular fa-10x"></i></br>
+			    	<label class="black">AngularJS Documentation</label>
+			    </a></div>
+			    <div class="col-sm center"><a href="https://www.typescriptlang.org/docs/home.html">
+					<img src="https://raw.githubusercontent.com/remojansen/logo.ts/master/ts.png" style="max-width: 45%"></br>
+			    	<label class="black">TypeScript Documentation</label>
+			    </a></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ other/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MAMP Server</title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+	<style type="text/css">
+		.padding 	{ padding 		: 2em; }
+		.center 	{ text-align	: center; }
+		.smallerText{ font-size 	: small; }
+		.margin 	{ margin-bottom : 1em }
+		.shortcuts 	{ position		: absolute;
+									  top: 8%;
+									  right: 5%; }
+	</style>
+</head>
+<body>
+	<div class="padding">
+		<h1 class="center">Welcome on your MAMP server.</h1>
+
+		</br><h6>Choose the project you want to run on the server. Click to open <a href="http://localhost:8888/phpMyAdmin/">phpMyAdmin</a> <!-- or <a href="./devTools.php">developement tools</a>-->.</h6>
+
+		<a class="btn btn-primary shortcuts" href="./devTools.php">Shortcuts</a>
+
+		<ul class="list-group z1">
+			<?php
+
+			$dir = dir('.');
+			
+			while($entry = $dir->read()) {
+			    if(stristr($entry, '.') === FALSE) {
+			    	echo '<li class="list-group-item"><a href="/' . $entry . '">' . $entry . '</a></li>';
+			    }
+			}
+
+			$dir->close();	 ?>
+
+			</br><h6>To add a new project, create a folder in :</h6>
+			<nav aria-label="breadcrumb">
+			  <ol class="breadcrumb">
+			    <li class="breadcrumb-item">Applications</li>
+			    <li class="breadcrumb-item">MAMP</li>
+			    <li class="breadcrumb-item active" aria-current="page">htdocs</li>
+			  </ol>
+			</nav>
+			<p class="font-weight-light smallerText">Don't forget to create 'index.php' to automatically start the PHP project.</p>
+		</ul>
+	</div>
+</body>
+</html>
